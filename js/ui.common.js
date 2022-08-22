0814e70d3668e8d8e167d066ec4a82587ca0d623
asever77 <johyunmin77@gmail.com>
2022-08-22 16:26:37 +0900

    메뉴설정

--- js/ui.common.js
+++ js/ui.common.js
@@ -3,6 +3,7 @@
 	'use strict';
 	
 	laosUI.common = {
+		isHeader: false,
 		init: function(){
 			laosUI.ajax.init({ 
 				area: document.querySelector('header.header'), 
@@ -21,11 +22,31 @@
 		},
 		header: function(){
 			console.log('header load');	
+			laosUI.common.isHeader = true;
 		},
 		footer: function(){
 			console.log('footer load');	
+		},
+		menuActive: function(v){
+			const el_sub = doc.querySelectorAll('.link-sub-gnb');
+			const len = el_sub.length;
+
+			if (!laosUI.common.isHeader) {
+				setTimeout(function(){
+					laosUI.common.menuActive(v);
+				},100)
+			} else {
+				for (let i = 0; i < len; i++) {
+					const txt = el_sub[i].innerText;
+
+					if (txt === v) {
+						el_sub[i].classList.add('active');
+						const el_wrap = el_sub[i].closest('.gnb-item');
+						el_wrap.classList.add('active');
+					}
+				}
+			}
 		}
-		
 	};
 
 	
